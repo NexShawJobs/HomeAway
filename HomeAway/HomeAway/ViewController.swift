26ce6aeab1becba022f51e0466237b4978223012
Nex <nexsoftnetwork@gmail.com>
2019-09-25 14:41:34 -0500

    create project

---
+++ HomeAway/HomeAway/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeAway
+//
+//  Created by Nex on 9/25/19.
+//  Copyright © 2019 nsn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
